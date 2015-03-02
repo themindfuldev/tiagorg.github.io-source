e1518819b1737f27252ff8a99b2f26d74c080938
Tiago Garcia <tiagoromerogarcia@gmail.com>
2015-03-01 18:08:47 -0800

    adding google analytics

--- themes/tiagorg/source/js/main.js
+++ themes/tiagorg/source/js/main.js
@@ -58,10 +58,21 @@ var main = (function() {
     $('#content article a').attr('target', '_blank');
   }
 
+  function setupGoogleAnalytics() {
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-60254345-1', 'auto');
+    ga('send', 'pageview');
+  }
+
   return {
     initialize: function() {
       setupGithubCommits();
       fixTargetLinks();
+      setupGoogleAnalytics();
     }
   }
 })();
